459f048bc40b770fcb4f7cc6f523ef810aa29dd4
Maysara <maysaracs1001@gmail.com>
2025-12-04 16:54:33 +0300

    check tailwind error

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,17 @@
+import logo from "./logo.webp";
+import gradientBackground from "./gradientBackground.png";
+import user_group from "./user_group.png";
+import star_icon from "./star_icon.svg";
+import star_dull_icon from "./star_dull_icon.svg";
+import profile_img_1 from "./profile_img_1.png";
+import arrow_icon from "./arrow_icon.svg";
+
+export const assets = {
+    logo,
+    gradientBackground,
+    user_group,
+    star_icon,
+    star_dull_icon,
+    profile_img_1,
+    arrow_icon,
+};
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import Hero from '../components/Hero.jsx'
+import Testimonial from '../components/Testimonial.jsx'
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <Testimonial />
+        </>
+    )
+}
+
+export default Home
